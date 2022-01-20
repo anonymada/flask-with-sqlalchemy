d33da2ec61366e9c9dfeb59fa3969f8dae0ff88b
Jacky HERINAVALONA <bmwailers@gmail.com>
2022-01-20 12:51:19 +0300

    First Deployment to Heroku

---
+++ wsgi.py
@@ -0,0 +1,26 @@
+# wsgi.py
+# pylint: disable=missing-docstring
+BASE_URL = '/api/v1'
+from flask import Flask
+from config import Config
+from flask_migrate import Migrate
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow  # NEW LINE (L'ordre est important ici !)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)  # NEW LINE
+
+from models import Product
+from schemas import many_product_schema
+
+migrate = Migrate(app, db)
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello World!", 200
+
+@app.route(f'{BASE_URL}/products', methods=['GET'])
+def get_many_product():
+    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    return many_product_schema.jsonify(products), 200
